bfaadf42d5b7e62a9456c6f89139b8fb1021a916
pathjo0826 <pathjo0826@skola.goteborg.se>
2022-04-20 13:25:45 +0200

    Adding unfinished work on emitting from dataComponent

--- JAVA-UX-GRUPP5/src/App.vue
+++ JAVA-UX-GRUPP5/src/App.vue
@@ -1,17 +1,22 @@
 <script>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import dataComponent from "./components/dataComponent.vue"
+import dataComponent from "./components/dataComponent.vue";
 export default {
   components: {
     dataComponent,
+  },
+  data() {
+    return {
+      selectedStationHref: "", //Fix this
+    };
   },
 };
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <data-component />
+  <data-component @stationHref="selectedStationHref" />
 </template>
 
 <style>
--- JAVA-UX-GRUPP5/src/components/dataComponent.vue
+++ JAVA-UX-GRUPP5/src/components/dataComponent.vue
@@ -2,25 +2,39 @@
   <p>Häst</p>
   <p>Hund</p>
   <input type="button" value="Tryck" @click="fetchData" />
-  <p v-for="(resource, index) in metObs.resource" :key="index">
-    {{ resource.title }}
-    <br />
-    {{ resource.link[0].href }}
-  </p>
+  <select @change="emitDataToParent(value)" v-model="value">
+    <option
+      :value="`${resource.link[0].href}`"
+      v-for="(resource, index) in metObs.resource"
+      :key="index"
+    >
+      {{ resource.title }}
+      <br />
+      {{ resource.summary }}
+      <br />
+      {{ resource.link[0].href }}
+    </option>
+  </select>
 </template>
 
 <script>
 import smhiService from "./../services/smhiService.js";
 export default {
   name: "dataComponent",
+  emits: ["stationHref"],
+
   data() {
     return {
       metObs: "",
+      value: "",
     };
   },
   methods: {
     async fetchData() {
-      this.metObs= await smhiService.fetchData();
+      this.metObs = await smhiService.fetchData();
+    },
+    emitDataToParent() {
+      this.$emit("stationHref", this.value);
     },
   },
 };
