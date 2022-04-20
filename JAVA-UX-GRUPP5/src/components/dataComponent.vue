de14ae44c76ae1cc30ced15d2463ed21aac0bfc1
pathjo0826 <pathjo0826@skola.goteborg.se>
2022-04-20 11:46:42 +0200

    starting out

--- JAVA-UX-GRUPP5/src/components/dataComponent.vue
+++ JAVA-UX-GRUPP5/src/components/dataComponent.vue
@@ -1,4 +1,26 @@
 <template>
-<p>Häst</p>
-<p>Hund</p>
+  <p>Häst</p>
+  <p>Hund</p>
+  <input type="button" value="Tryck" @click="fetchData" />
+  <p v-for="(index, resource) in metObs.resource" :key="index">{{resource.summary}}</p>
 </template>
+
+<script>
+export default {
+  name: "dataComponent",
+  data() {
+    return {
+      metObs: ""
+    };
+  },
+  methods: {
+    async fetchData() {
+      let response = await fetch(
+        "http://opendata-download-metobs.smhi.se/api/version/1.0.json"
+      );
+      this.metObs = await response.json();
+      console.log(this.metObs);
+    },
+  },
+};
+</script>
