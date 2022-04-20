fb9fe038d7883d2539413453d1d4b8fbbdde0921
pathjo0826 <pathjo0826@skola.goteborg.se>
2022-04-20 12:45:13 +0200

    adding things

--- JAVA-UX-GRUPP5/src/components/dataComponent.vue
+++ JAVA-UX-GRUPP5/src/components/dataComponent.vue
@@ -2,24 +2,25 @@
   <p>Häst</p>
   <p>Hund</p>
   <input type="button" value="Tryck" @click="fetchData" />
-  <p v-for="(resource, index) in metObs.resource" :key="index">{{resource.title}}</p>
+  <p v-for="(resource, index) in metObs.resource" :key="index">
+    {{ resource.title }}
+    <br />
+    {{ resource.link[0].href }}
+  </p>
 </template>
 
 <script>
+import smhiService from "./../services/smhiService.js";
 export default {
   name: "dataComponent",
   data() {
     return {
-      metObs: ""
+      metObs: "",
     };
   },
   methods: {
     async fetchData() {
-      let response = await fetch(
-        "http://opendata-download-metobs.smhi.se/api/version/1.0.json"
-      );
-      this.metObs = await response.json();
-      console.log(this.metObs);
+      this.metObs= await smhiService.fetchData();
     },
   },
 };
