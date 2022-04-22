70fe55b57f82bcab542556aa228448d47e1cebc0
Magnus Lilja <magnus@connoisseur.se>
2022-04-22 12:51:39 +0200

    started om periodComp

---
+++ JAVA-UX-GRUPP5/src/components/periodComponent.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="period-component-container">
+    <div>periodComponent</div>
+    <!--    <div>Välj station:</div>-->
+    <!--    <select-->
+    <!--        v-model="selectedStationHref"-->
+    <!--        class=""-->
+    <!--        @change="toParent(selectedStationHref)">-->
+    <!--      <option-->
+    <!--          v-for="(station, index) in stationsForParam.station"-->
+    <!--          :key="`${index}`"-->
+    <!--          :value="{data: station.link[0].href}"-->
+    <!--      >-->
+    <!--        {{ station.name }}-->
+    <!--      </option>-->
+    <!--    </select>-->
+  </div>
+</template>
+
+<script>
+import smhiService from "./../services/smhiService.js";
+
+export default {
+  props: {},
+  emits: [''],
+  data() {
+    return {}
+  },
+  methods: {},
+  watch: {}
+}
+</script>
+
+<style scoped>
+.period-component-container {
+  background-color: #d6ded1;
+  padding: 2em;
+}
+</style>
