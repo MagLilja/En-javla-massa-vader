eecc6a3d45da9d5c7152c547ef86dc41816a77f8
Magnus Lilja <magnus@connoisseur.se>
2022-04-22 12:51:52 +0200

    added error handling

--- JAVA-UX-GRUPP5/src/services/smhiService.js
+++ JAVA-UX-GRUPP5/src/services/smhiService.js
@@ -2,7 +2,12 @@ const smhiService = {
 
     async fetchData(URL = "https://opendata-download-metobs.smhi.se/api/version/1.0.json") {
         let response = await fetch(URL);
-        return response.json();
+        if (response.status === 200) {
+            console.log("response OK");
+            console.log(response);
+            return response.json();
+        }
+        return response.status
     },
 }
 Object.freeze(smhiService)
