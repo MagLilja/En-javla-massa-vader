38b57c793e6c1a1b0cc02399446264a614fee9ff
pathjo0826 <pathjo0826@skola.goteborg.se>
2022-04-27 12:26:42 +0200

    Adding css for active state navbar items

--- JAVA-UX-GRUPP5/src/components/NavbarComponent.vue
+++ JAVA-UX-GRUPP5/src/components/NavbarComponent.vue
@@ -1,33 +1,24 @@
 <template>
   <nav class="nav-bar-container">
-    <div class="nav-bar-item">
-      <router-link to="/"
-        ><img src="./../assets/icons/main-view-icon.svg" alt="start-view-icon"
-      /></router-link>
-    </div>
+    <router-link class="nav-bar-item" to="/"
+      ><img src="./../assets/icons/main-view-icon.svg" alt="start-view-icon"
+    /></router-link>
 
-    <div class="nav-bar-item">
-      <router-link to="/ten-day-forecast"
-        ><img src="./../assets/icons/10-day-view.svg" alt="10-day-view"
-      /></router-link>
-    </div>
+    <router-link class="nav-bar-item" to="/ten-day-forecast"
+      ><img src="./../assets/icons/10-day-view.svg" alt="10-day-view"
+    /></router-link>
 
-    <div class="nav-bar-item">
-      <router-link to="/summary"
-        ><img src="./../assets/icons/summary-view.svg" alt="summary-view"
-      /></router-link>
-    </div>
+    <router-link class="nav-bar-item" to="/summary"
+      ><img src="./../assets/icons/summary-view.svg" alt="summary-view"
+    /></router-link>
 
-    <div class="nav-bar-item">
-      <router-link to="/worst-weather"
-        ><img src="./../assets/icons/worst-weather.svg" alt="worst-weather"
-      /></router-link>
-    </div>
-    <div class="nav-bar-item">
-      <router-link to="favorite"
-        ><img src="./../assets/icons/favorite.svg" alt="favorite"
-      /></router-link>
-    </div>
+    <router-link class="nav-bar-item" to="/worst-weather"
+      ><img src="./../assets/icons/worst-weather.svg" alt="worst-weather"
+    /></router-link>
+
+    <router-link class="nav-bar-item" to="favorite"
+      ><img src="./../assets/icons/favorite.svg" alt="favorite"
+    /></router-link>
   </nav>
 </template>
 
@@ -44,12 +35,15 @@ export default {};
   width: 100vw;
 }
 .nav-bar-item {
-    padding: 0.2em;
+  padding: 0.2em;
   margin: 0.2em;
+  border: 2px solid lightgrey;
+}
+.router-link-active {
+  height: 100%;
   border: 2px solid black;
 }
 .nav-bar-item img {
-
   width: 5rem;
 }
 </style>
