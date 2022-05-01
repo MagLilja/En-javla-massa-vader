0db24ea6c267c2b650fea0d1c53d8fd66fa60f43
Magnus Lilja <magnus@connoisseur.se>
2022-05-01 19:55:26 +0200

    testing rotating pointer

--- JAVA-UX-GRUPP5/src/components/start/CircleComponent.vue
+++ JAVA-UX-GRUPP5/src/components/start/CircleComponent.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <div class="circle-container">
-      <div class="outer-circle-container">
+      <div class="outer-circle-container" :style="{'--my-var': bgColor}">
         <div class="outer-circle-item" v-for="(wx, index) of completeDailyWxList" :key="index">
           {{ wx.time }}
           <br>{{ wx.wSymb2Symbol }}
           <br>{{ wx.temperature }}&#176C
         </div>
-        <div class="inner-circle-container" :style="{}">
+        <div class="inner-circle-container" >
           <div class="inner-circle-temperature">{{
               forecastFullData.timeSeries[0].parameters[10].values[0]
             }}&#176C
@@ -18,9 +18,9 @@
           </div>
           <div class="inner-circle-time">
 
-<!--            {{ seTime(forecastFullData.timeSeries[0].validTime) }}-->
-            {{nowTime()}}</div>
-          <div class="hours"></div>
+            <!--            {{ seTime(forecastFullData.timeSeries[0].validTime) }}-->
+            {{ nowTime() }}
+          </div>
         </div>
       </div>
     </div>
@@ -38,6 +38,7 @@ export default {
   data() {
     return {
       wSymb2Decoder: WSymb2,
+      bgColor: "90deg",
     }
   },
   watch: {},
@@ -50,9 +51,9 @@ export default {
       }
       return event.toLocaleTimeString('se-SV', options)
     },
-    nowTime(){
+    nowTime() {
       let now = new Date()
-      return now.getHours() +":"+ now.getMinutes()
+      return now.getHours() + ":" + now.getMinutes()
     },
     getWSymb2Unicode(data) {
       for (const wSymb2 of this.wSymb2Decoder.weathers) {
@@ -60,47 +61,85 @@ export default {
           return wSymb2.symbol
         }
       }
+    },
+    initLocalClocks() {
+      // Get the local time using JS
+      var date = new Date;
+      var seconds = date.getSeconds();
+      var minutes = date.getMinutes();
+      var hours = date.getHours();
+
+      // Create an object with each hand and it's angle in degrees
+      var angle = (hours * 30) + (minutes / 2)
+
+
+
+      // Loop through each of these hands to set their angle
+      for (var j = 0; j < hands.length; j++) {
+        var elements = document.querySelectorAll('.' + hands[j].hand);
+        for (var k = 0; k < elements.length; k++) {
+          elements[k].style.webkitTransform = 'rotateZ(' + hands[j].angle + 'deg)';
+          elements[k].style.transform = 'rotateZ(' + hands[j].angle + 'deg)';
+          // If this is a minute hand, note the seconds position (to calculate minute position later)
+          if (hands[j].hand === 'minutes') {
+            elements[k].parentNode.setAttribute('data-second-angle', hands[j + 1].angle);
+          }
+        }
+      }
     }
   }
 }
 </script>
 
-<style>
+<style scoped>
 
-.hours {
-  background: #be1a1a;
-  border:1px solid #801111;
-  height: 50px;
-  left: 48.75%;
-  position: absolute;
-  top: 0%;
-  transform-origin: 50% 100%;
-  width: 1%;
-  z-index:0;
-}
+
 @keyframes rotate {
   100% {
-    transform: rotateZ(360deg);
+    /*transform: rotateZ(360deg);*/
+    rotate: 360deg;
   }
 }
 
 .circle-container {
-  display: flex;
-  justify-content: center;
+  display: grid;
+  place-items: center;
   width: 300px;
-  animation: rotate 60s infinite linear;
+  /*overflow: hidden;*/
 }
 
+.outer-circle-container::after {
+  content: "";
+  /*En gradient för att få änden av det roterande objektet att "försvinna"*/
+  /*background: linear-gradient(45deg, rgba(230, 230, 230, 0) 0%,*/
+  /*rgba(231, 71, 71, 0) 35%,*/
+  /*rgba(0, 212, 255, 1) 100%);;*/
+
+  background-image: url('../../assets/icons/arrow-up-solid.svg');
+background-size: 1em;
+  background-repeat: no-repeat;
+  height: 230px;
+  position: absolute;
+  --my-var:0deg;
+  transform: rotateZ(var(--my-var));
+  width: 16px;
+  /*border:1px solid black;*/
+  border-radius: 12px;
+  z-index: 1;
+  animation: rotate 600s infinite linear;
+}
+
+
 .outer-circle-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  display: grid;
+  place-items: center;
   width: 300px;
   height: 300px;
   border: 1px solid #afafaf;
   border-radius: 50%;
   position: relative;
   background-color: #e6e6e6;
+  overflow: hidden;
 }
 
 .outer-circle-item {
@@ -110,61 +149,61 @@ export default {
 }
 
 .outer-circle-item:nth-child(1) {
-  top: calc(150px - 150*1px + 7px);
+  top: calc(150px - 150 * 1px + 7px);
 }
 
 .outer-circle-item:nth-child(2) {
-  top: calc(150px - 150*0.86px + 2px);
-  right: calc(150px - 150*0.5px);
+  top: calc(150px - 150 * 0.86px + 2px);
+  right: calc(150px - 150 * 0.5px);
 }
 
 .outer-circle-item:nth-child(3) {
-  top: calc(150px - 150*0.5px - 10px);
-  right: calc(150px - 150*0.86px + 10px);
+  top: calc(150px - 150 * 0.5px - 10px);
+  right: calc(150px - 150 * 0.86px + 10px);
 }
 
 .outer-circle-item:nth-child(4) {
-  top: calc(150px - 150*0px - 20px);
-  right: calc(150px - 150*1px + 12px);
+  top: calc(150px - 150 * 0px - 20px);
+  right: calc(150px - 150 * 1px + 12px);
 }
 
 .outer-circle-item:nth-child(5) {
-  bottom: calc(150px - 150*0.5px - 10px);
-  right: calc(150px - 150*0.86px + 8px);
+  bottom: calc(150px - 150 * 0.5px - 10px);
+  right: calc(150px - 150 * 0.86px + 8px);
 }
 
 .outer-circle-item:nth-child(6) {
-  bottom: calc(150px - 150*0.86px);
-  right: calc(150px - 150*0.5px);
+  bottom: calc(150px - 150 * 0.86px);
+  right: calc(150px - 150 * 0.5px);
 }
 
 .outer-circle-item:nth-child(7) {
-  bottom: calc(150px - 150*1px + 5px);
+  bottom: calc(150px - 150 * 1px + 5px);
 }
 
 .outer-circle-item:nth-child(8) {
-  bottom: calc(150px - 150*0.86px);
-  left: calc(150px - 150*0.5px);
+  bottom: calc(150px - 150 * 0.86px);
+  left: calc(150px - 150 * 0.5px);
 }
 
 .outer-circle-item:nth-child(9) {
-  bottom: calc(150px - 150*0.5px - 10px);
-  left: calc(150px - 150*0.86px + 8px);
+  bottom: calc(150px - 150 * 0.5px - 10px);
+  left: calc(150px - 150 * 0.86px + 8px);
 }
 
 .outer-circle-item:nth-child(10) {
-  top: calc(150px - 150*0px - 20px);
-  left: calc(150px - 150*1px + 12px);
+  top: calc(150px - 150 * 0px - 20px);
+  left: calc(150px - 150 * 1px + 12px);
 }
 
 .outer-circle-item:nth-child(11) {
-  top: calc(150px - 150*0.5px - 10px);
-  left: calc(150px - 150*0.86px + 10px);
+  top: calc(150px - 150 * 0.5px - 10px);
+  left: calc(150px - 150 * 0.86px + 10px);
 }
 
 .outer-circle-item:nth-child(12) {
-  top: calc(150px - 150*0.86px + 2px);
-  left: calc(150px - 150*0.5px);
+  top: calc(150px - 150 * 0.86px + 2px);
+  left: calc(150px - 150 * 0.5px);
 }
 
 
@@ -180,17 +219,18 @@ export default {
   border: 1px solid #be1a1a;
   border-radius: 50%;
   background-color: #adabab;
+  z-index: 2;
 }
 
 .inner-circle-temperature {
   font-size: 2.5em;
 }
 
-.inner-circle-time{
-  font-size:2em;
+.inner-circle-time {
+  font-size: 2em;
 }
 
 .inner-circle-symbol {
-  font-size:2em;
+  font-size: 2em;
 }
 </style>
