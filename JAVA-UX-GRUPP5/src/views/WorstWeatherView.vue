8bb39f06cd8bae17ab69ef77697bebda5e24439e
pathjo0826 <pathjo0826@skola.goteborg.se>
2022-05-11 14:39:09 +0200

    dum ideas

--- JAVA-UX-GRUPP5/src/views/WorstWeatherView.vue
+++ JAVA-UX-GRUPP5/src/views/WorstWeatherView.vue
@@ -15,16 +15,28 @@ export default {
   },
   methods: {
     async getMultiPointAnalysis() {
+      /*
       let validTimeData = await smhiService.fetchData("https://opendata-download-metanalys.smhi.se/api/category/mesan1g/version/2/validtime.json")
       let validTime = validTimeData.validTime[0];
-      let p = "t"
+      let p = "t" 
       let lt = "hl"
       validTime = validTime.replace(/[:-]/g, "");
       let l = 2
-      let url = `https://opendata-download-metanalys.smhi.se/api/category/mesan1g/version/2/geotype/multipoint/validtime/${validTime}/parameter/${p}/leveltype/${lt}/level/${l}/data.json?with-geo=false&downsample=2`
+      let url = `https://opendata-download-metanalys.smhi.se/api/category/mesan1g/version/2/geotype/multipoint/validtime/${validTime}/parameter/${p}/leveltype/${lt}/level/${l}/data.json?downsample=2`
       let data = await smhiService.fetchData(url);
-      console.log(data);
-
+      console.log(data)
+        console.log(data.geometry.coordinates[0][0]);
+        console.log(data.geometry.coordinates[0][1]);
+      console.log(data.timeSeries[0].parameters[0].values[0]);
+      */
+     
+     // Tar fram polygonens hörn. Oanvändbart.
+     
+     let category = "mesan1g"
+     let version = "2"
+     let url2 = `https://opendata-download-metanalys.smhi.se/api/category/${category}/version/${version}/geotype/polygon.json`
+     let data = await smhiService.fetchData(url2);
+     console.log(data)
 
       // /api/category/mesan1g/version/2/geotype/multipoint/validtime/20151012T150000Z/parameter/t/leveltype/hl/level/2/data.json?with-geo=false
     }
