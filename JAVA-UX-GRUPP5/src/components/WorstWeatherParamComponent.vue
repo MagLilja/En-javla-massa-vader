21bbe994adebb2a2a38c1f288ba13b1aaebdb2c5
Magnus Lilja <magnus@connoisseur.se>
2022-05-12 18:24:37 +0200

    work on worst weather

--- JAVA-UX-GRUPP5/src/components/WorstWeatherParamComponent.vue
+++ JAVA-UX-GRUPP5/src/components/WorstWeatherParamComponent.vue
@@ -2,7 +2,7 @@
   <div class="hottest grid grid-cols-2 justify-items-start w-fit gap-4">
     <div class="font-bold w-fit col-span-2 justify-self-center">{{ title }}</div>
     <!--      <div></div>-->
-    <div class="justify-self-end"><img class="w-16" :src="icon" alt=""></div>
+    <div class="justify-self-end"><img class="w-16" :src="iconName"  alt=""></div>
     <div class="max-w-max justify-self-start flex flex-col">
       <div class="">{{ parameter.city.minValueGeo.features[0].properties.city }}</div>
       <div class="self-start">{{ parameter.minValue }} {{ parameter.unit }}
@@ -11,17 +11,17 @@
   </div>
 </template>
 <script>
+ import icon from "@/assets/prototype_icons/snowflake-solid3.svg"
 
 export default {
   name: 'worst-weather-param-component',
   props: {
     parameter: {},
-    title:String,
-    icon: String
+    title: String,
   },
   data() {
     return {
-      icon:"src/assets/prototype_icons/snowflake-solid3.svg"
+      iconName:icon
     }
   },
 }
