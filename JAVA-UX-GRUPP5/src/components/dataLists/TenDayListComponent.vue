a5bae609ca6f061746a252ddbb714f09146be44a
Magnus Lilja <magnus@connoisseur.se>
2022-05-13 10:39:05 +0200

    style ten day list

--- JAVA-UX-GRUPP5/src/components/dataLists/TenDayListComponent.vue
+++ JAVA-UX-GRUPP5/src/components/dataLists/TenDayListComponent.vue
@@ -5,19 +5,24 @@
     <div class="w-fit">Tid</div>
     <div class="">Väder</div>
     <div>Temperatur</div>
-    <div class="col-span-2 place-self-end">Vind</div>
+    <div class="col-span-2 justify-self-end">Vind</div>
     <div class="border-b-2 col-span-5 my-2"></div>
 
     <template class="" v-for="(wd,index) of weatherData" :key="index">
       <div class="">{{ getLocalDate(wd.dataDate) }}</div>
-      <div class="">{{ wd.highestTemp }}&#176 ({{ wd.lowestTemp }}&#176)</div>
+      <div class="flex flex-col ">
+        <div class="">{{ wd.highestTemp }}&#176</div>
+        <div class="">({{ wd.lowestTemp }}&#176)</div>
+      </div>
       <div class="content-start items-start justify-start">{{ wd.totalPrecipitation }} mm</div>
-      <div class="col-span-2 place-self-end">{{ wd.roundedAvgWindSpeed }} m/s ({{ wd.highestGust }}) m/s</div>
+      <div class="col-span-2 justify-self-end flex flex-col items-end">
+        <div class="x"> {{ wd.roundedAvgWindSpeed }} m/s</div>
+        <div class="x">({{ wd.highestGust }}) m/s</div>
+      </div>
       <div class="border-b-2 col-span-5 my-2 "></div>
     </template>
 
-  </div >
-
+  </div>
 
 
   <!--
@@ -40,42 +45,41 @@
   -->
 </template>
 <script>
-import {mapState} from "pinia/dist/pinia";
-import {useUserDataStore} from "@/stores/useUserDataStore";
-import weatherDataManager from "@/managers/WeatherDataManager";
+  import { mapState } from "pinia/dist/pinia";
+  import { useUserDataStore } from "@/stores/useUserDataStore";
+  import weatherDataManager from "@/managers/WeatherDataManager";
 
-export default {
-  name: 'ten-day-list-component',
-  data() {
-    return {
-      weatherData: [],
-    }
-  },
-  computed: {
-    ...mapState(useUserDataStore, ["getCoordinates", "getForecastFullData"])
-  },
-  created() {
-    for (let i = 0; i < 10; i++) {
-      let date = new Date()
-      //date = date.getDate + i
-      date.setDate(date.getDate() + i)
-      
-      this.weatherData.push(weatherDataManager.getWeatherDataforDate(this.getForecastFullData, date))
-
-    }
-  },
-  methods: {
-    getLocalDate(input) {
-      const event = new Date(input);
-      const options = { weekday: "long", month: "long", day: "numeric" };
-      const date = event.toLocaleDateString("sv-SE", options);
-      return date.replace(date.charAt(0), date.charAt(0).toUpperCase())
+  export default {
+    name: "ten-day-list-component",
+    data() {
+      return {
+        weatherData: []
+      };
     },
-  }
-}
+    computed: {
+      ...mapState(useUserDataStore, ["getCoordinates", "getForecastFullData"])
+    },
+    created() {
+      for (let i = 0; i < 10; i++) {
+        let date = new Date();
+        //date = date.getDate + i
+        date.setDate(date.getDate() + i);
+
+        this.weatherData.push(weatherDataManager.getWeatherDataforDate(this.getForecastFullData, date));
+
+      }
+    },
+    methods: {
+      getLocalDate(input) {
+        const event = new Date(input);
+        const options = { weekday: "long", month: "long", day: "numeric" };
+        const date = event.toLocaleDateString("sv-SE", options);
+        return date.replace(date.charAt(0), date.charAt(0).toUpperCase());
+      }
+    }
+  };
 </script>
 <style>
-
 
 
 </style>
